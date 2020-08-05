6685458ac7b152fa9a4891035c7ba5f82aa8932a
chris48s <chris.shaw480@gmail.com>
2020-08-05 19:14:03 +0100

    [DEPLOY] Add built site

---
+++ component---frontend-pages-index-tsx-492ea2e4e5188709c2f5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"Qj/l":function(n,u,w){"use strict";w.r(u);var a=w("2juE");u.default=a.default}}]);
+//# sourceMappingURL=component---frontend-pages-index-tsx-492ea2e4e5188709c2f5.js.map
