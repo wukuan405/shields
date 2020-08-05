4c915b6e7a8856ff824e70cd23608c91a5e85745
chris48s <chris.shaw480@gmail.com>
2020-08-05 19:14:03 +0100

    [DEPLOY] Completely clean the index

--- core/server/instance-id-generator.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-function generateInstanceId() {
-  // from https://gist.github.com/gordonbrander/2230317
-  return Math.random().toString(36).substr(2, 9)
-}
-
-module.exports = generateInstanceId
--- services/jira/jira-common.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const authConfig = {
-  userKey: 'jira_user',
-  passKey: 'jira_pass',
-  serviceKey: 'jira',
-}
-
-module.exports = { authConfig }
